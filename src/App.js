f0f5e6691efb00b3095dc5aea6fbd4b7ad1307ca
Your Name <you@example.com>
2021-04-30 11:18:29 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import Posts from "./components/posts"
-
+import { Route, Switch } from "react-router-dom";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import notFound from "./components/notFound";
 function App() {
-
-   return (
-     <div>
-       <Posts/>
-
-
+ return (
+     <div className="container-fluid">
+       <NavBar />
+       <div className="container">
+           <div className="content">
+               <Switch>
+                   <Route path="/posts"
+                   render={props => <Posts sortBy="newest" {...props} />}
+                   />
+                   <Route path="/" exact component={Home} />
+               </Switch>
+           </div>
+       </div>
      </div>
+ );
+}
 
-   );
-  }
 
 
 
